2ba8bd1c2c5edd0c0ad22aff9965781dbbc666c8
Виталий Анатольевич <pigcontrol@gmail.com>
2020-06-28 13:33:19 +0300

    2

--- Program.cs
+++ Program.cs
@@ -15,15 +15,24 @@ namespace Discount1
         static void Main(string[] args)
         {
             float cost;
+            float discount;
 
             Console.WriteLine("Вычисление стоимости покупки с учетом скидки.");
             Console.WriteLine("Скидка в 10 % предоставляется, если сумма покупки больше 1000 грн.");
             Console.WriteLine("Введите сумму покупки и нажмите <Enter>");
             cost = float.Parse(Console.ReadLine().Replace(",", "."));
 
+            Console.WriteLine("-----------------------------------------------");
+
             if (cost >= 1000)
             {
-
+                discount = (cost * 90) / 100;
+                Console.WriteLine("Вам предоставляется скидка 10%");
+                Console.WriteLine($"Сумма покупки с учетом скидки: {discount} грн.");
+            }
+            else
+            {
+                Console.WriteLine("Сумма покупки меньше 1000 грн. Cкидка не предоставляется.");
             }
 
             Console.ReadLine();
